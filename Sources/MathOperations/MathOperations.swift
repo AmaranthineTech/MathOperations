5f505613c7710545f233fa38d70afa8bbe32a242
arunrpatwardhan <37834476+arunrpatwardhan@users.noreply.github.com>
2018-04-04 17:04:13 +0530

    Add files via upload

---
+++ Sources/MathOperations/MathOperations.swift
@@ -0,0 +1,31 @@
+import ErrorTypes
+
+public struct MathOperations
+{
+	static func add(Number num1 : Int, with num2 : Int) -> Int
+	{
+		return num1 + num2
+	}
+
+	static func mult(Number num1 : Int, with num2 : Int) -> Int
+	{
+		return num1 * num2
+	}
+
+	static func div(Number num1 : Int, by num2 : Int) throws -> Int
+	{
+		guard num2 > 0
+		else
+		{
+			throw ErrorCodes.DivideByZero("You are dividing by zero. Inthe second argument is incorrect.")
+		}
+
+		return num1 / num2
+	}
+
+	static func sub(_ num1 : Int, from num2 : Int) -> Int
+	{
+		return num2 - num1
+	}
+}
+
